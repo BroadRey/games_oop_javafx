fc9eabb3dd91afbdde3e564df8a63a60286cc414
andrey <larginator@gmail.com>
2023-07-16 16:42:13 +0600

    3. Каркас шахматной доски [#792]

---
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.chess;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LogicTest {
+    @Test
+    public void whenMoveThenFigureNotFoundException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenWayOccupiedThenOccupiedCellException() {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C8));
+        logic.add(new PawnBlack(Cell.D7));
+        assertThrows(OccupiedCellException.class,
+                () -> {
+                    logic.move(Cell.C8, Cell.E6);
+                });
+    }
+}
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class BishopBlackTest {
+    @Test
+    void WhenReturnCorrectPosition() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        Cell expected = Cell.C8;
+        Cell actual = bishopBlack.position();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void WhenReturnCorrectCopy() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        Cell expectedPosition = Cell.D7;
+        Figure newBishopBlack = bishopBlack.copy(expectedPosition);
+        assertThat(newBishopBlack).isNotEqualTo(bishopBlack);
+        assertThat(newBishopBlack.position()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void WhenReturnCorrectWayFromC1ToG5() {
+        Cell sourcePosition = Cell.C1;
+        Cell destPosition = Cell.G5;
+        BishopBlack bishopBlack = new BishopBlack(sourcePosition);
+        Cell[] expectedPWay = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Cell[] actualWay = bishopBlack.way(destPosition);
+        assertThat(actualWay).isEqualTo(expectedPWay);
+    }
+
+    @Test
+    void WhenReturnIncorrectWayFromC1ToG4() {
+        Cell sourcePosition = Cell.C1;
+        Cell destPosition = Cell.G4;
+        BishopBlack bishopBlack = new BishopBlack(sourcePosition);
+        assertThatThrownBy(() -> bishopBlack.way(destPosition))
+                .isInstanceOf(ImpossibleMoveException.class)
+                .hasMessage(
+                        String.format("Could not move by diagonal from %s to %s",
+                                sourcePosition,
+                                destPosition)
+                );
+    }
+
+    @Test
+    void WhenCellsLayOnDiagonal() {
+        Cell sourcePosition = Cell.C8;
+        Cell destPosition = Cell.D7;
+        BishopBlack bishopBlack = new BishopBlack(sourcePosition);
+        boolean actualWay = bishopBlack.isDiagonal(bishopBlack.position(), destPosition);
+        assertThat(actualWay).isTrue();
+    }
+
+    @Test
+    void WhenCellsDontLayOnDiagonal() {
+        Cell sourcePosition = Cell.C8;
+        Cell destPosition = Cell.D8;
+        BishopBlack bishopBlack = new BishopBlack(sourcePosition);
+        boolean actualWay = bishopBlack.isDiagonal(bishopBlack.position(), destPosition);
+        assertThat(actualWay).isFalse();
+    }
+}
